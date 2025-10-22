17229d2aa83cf1ffd47770ff119c8999e8cb0c5c
abishek <abishekedu123@gmail.com>
2025-10-22 15:59:40 +0530

    k

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -100,4 +100,5 @@ export const login = async (req, res) => {
 export const logout = async (req, res) => {
   res.cookie("jwt", "", { maxAge: 0 });
   res.status(200).json({ message: "Logged out successfully" });
+
 };
