98db96ac5f02236c89472c5c27e4fc7c93bbc905
abishek <abishekedu123@gmail.com>
2025-10-22 13:42:41 +0530

    welcome-email implemented

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,7 +1,8 @@
 import jwt from "jsonwebtoken";
+import { ENV } from "../lib/env.js";
 
 export const generateToken = (userId,res) => {
-    const token = jwt.sign({ id: userId}, process.env.JWT_SECRET, {
+    const token = jwt.sign({ id: userId}, ENV.JWT_SECRET, {
         expiresIn: '7d',
     });
  
@@ -9,7 +10,7 @@ export const generateToken = (userId,res) => {
         maxAge: 7 * 24 * 60 *60 * 1000, //7 days
         httpOnly: true, //prevent xss attackers: cross-site scripting
         sameSite: "Strict", //CSRF attacks
-        secure: process.env.NODE_ENV === "development" ? false : true, //https ,
+        secure: ENV.NODE_ENV === "development" ? false : true, //https ,
     });
 
     return token;
