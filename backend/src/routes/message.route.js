f8ae4342d0c1ad05b121b281dc4b5c8497c9e495
abishek <abishekedu123@gmail.com>
2025-10-22 20:53:31 +0530

    message-Routes added

--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { getAllContacts , getMessageByUserId, sendMessage, getChatPartners} from "../controllers/message.controller.js";
+import { arcjetProtection } from './../middleware/arcjet.middleware.js';
 
 const router = express.Router();
 
-router.get("/send", (req,res) => {
-    res.send("Send message endpoint");
-})
+// Apply Arcjet protection and authentication middleware to all routes
+router.use(arcjetProtection, protectRoute);
+
+router.get("/contacts", getAllContacts);
+router.get("/chats", getChatPartners);
+router.get("/:id", getMessageByUserId);
+router.post("/send/:id", sendMessage);
 
 export default router;
