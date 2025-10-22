0fd948c6dda2eba3b35d609fe0859235d3c2cd47
abishek <abishekedu123@gmail.com>
2025-10-22 14:29:07 +0530

    login-logout implemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -52,11 +52,7 @@ export const signup = async (req, res) => {
       // todo: send welcome email to user
 
       try {
-        await sendWelcomeEmail(
-          newUser.email,
-          newUser.fullName,
-          ENV.CLIENT_URL
-        );
+        await sendWelcomeEmail(newUser.email, newUser.fullName, ENV.CLIENT_URL);
       } catch (error) {
         console.error("Error sending welcome email:", error);
       }
@@ -70,15 +66,34 @@ export const signup = async (req, res) => {
 };
 
 export const login = async (req, res) => {
-  const { fullName, email, password } = req.body;
+  const { email, password } = req.body;
 
   try {
-    if (!fullName || !email || !password) {
-      return res.status(400).json({ message: "All Fields Are Required" });
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
     }
-  } catch (error) {}
+    const isPasswordCorrect = await bcrypt.compare(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    generateToken(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+
+  } catch (error) {
+    console.error("Error in Login Endpoint:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
 };
 
 export const logout = async (req, res) => {
-  res.send("Logout endpoint");
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "Logged out successfully" });
 };
